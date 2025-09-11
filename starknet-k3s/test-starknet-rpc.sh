483ecff11e9702eba01d7ddb7289297b84228509
harodggg <haroldtiansheng@gmail.com>
2025-09-11 16:54:14 +0800

    feat: add attestion-deployment for starknet
    Signed-off-by: harodggg <haroldtiansheng@gmail.com>

---
+++ starknet-k3s/test-starknet-rpc.sh
@@ -0,0 +1,6 @@
+curl --request POST --header 'Content-Type: application/json' --data '{
+         "jsonrpc": "2.0",
+         "method": "starknet_blockNumber",
+         "params": [],
+         "id": 1
+     }' http://pathfinder.xxxyyy.space/rpc/v0_9
