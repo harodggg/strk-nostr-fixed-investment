3a5126590b3124de8a0c768260d3a76d90fc272f
harodggg <haroldtiansheng@gmail.com>
2025-09-13 12:29:38 +0800

    feat: add lend
    Signed-off-by: harodggg <haroldtiansheng@gmail.com>

--- nostr-reinvest/lend_usdt.py
+++ nostr-reinvest/lend_usdt.py
@@ -25,9 +25,10 @@ resource_bounds = ResourceBoundsMapping(
 
 the_lastlow_price = 100000  # 1str = 0.1usdt
 
-# nostr swap contract
-SWAP_CONTRACT = 0x040784ffdde08057a5957e64ed360c0ae4e04117b6d8e351c6bb912c09c5cbf5
 
+# nostr lend contract
+
+LEND_NOSTRA_CONTRACT = 0x0453c4c996f1047d9370f824d68145bd5e7ce12d00437140ad02181e1d11dc83
 
 STARK_CONTRACT = 0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d
 USDT_CONTRACT = 0x068F5c6a61780768455de69077E07e89787839bf8166dEcfBf92B645209c0fB8
@@ -47,29 +48,24 @@ async def lend_to_usdt(amount) -> Union[str, bool]:
     account = Account(
         address=MY_ACCOUNT_KEY["address"], client=client, key_pair=KeyPair.from_private_key(MY_ACCOUNT_KEY["private_key"]), chain=StarknetChainId.MAINNET)
    # Create contract from contract's address - Contract will download contract's ABI to know its interface.
-    strk_contract = await Contract.from_address(address=STARK_CONTRACT, provider=account)
+    usdt_contract = await Contract.from_address(address=USDT_CONTRACT, provider=account)
 
-    nostra_swap_contract = await Contract.from_address(address=SWAP_CONTRACT, provider=account)
+    nostra_lend_contract = await Contract.from_address(address=LEND_NOSTRA_CONTRACT, provider=account)
 
-    invocation_approve = strk_contract.functions["approve"].prepare_invoke_v3(
-        SWAP_CONTRACT, int(amount * pow(10, 18)))
+    invocation_approve = usdt_contract.functions["approve"].prepare_invoke_v3(
+        LEND_NOSTRA_CONTRACT, int(amount * pow(10, 6)))
 
-    invocation_swap = nostra_swap_contract.functions["swap_exact_tokens_for_tokens"].prepare_invoke_v3(
-        int(amount * pow(10, 18)),
-        int(the_lastlow_price * amount),
-        0x4718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d,
-        [
-            0x7ae43abf704f4981094a4f3457d1abe6b176844f6cdfbb39c0544a635ef56b0,
-            0xc318445d5a5096e2ad086452d5c97f65a9d28cafe343345e0fa70da0841295
-        ],
-        0x41c3e6df37144f2287924103d2ced9e2e83a945bb72febf2b4c8a5571a100ac,
-        int(time.time()) + 60)
+    invocation_lend = nostra_lend_contract.functions["mint"].prepare_invoke_v3(
+        MY_ACCOUNT_KEY['address'],
+        int(amount * pow(10, 6))
+
+    )
 
     transaction_response = await account.execute_v3(
-        calls=[invocation_approve, invocation_swap],
+        calls=[invocation_approve, invocation_lend],
         auto_estimate=True
     )
     print(transaction_response)
 
 if __name__ == '__main__':
-    asyncio.run(lend_to_usdt(0.1))
+    asyncio.run(lend_to_usdt(0.01))
