cf307318e37ec7a51494fdf863812795b530aee4
harodggg <haroldtiansheng@gmail.com>
2025-09-17 01:44:50 +0800

    feat: add  logger
    Signed-off-by: harodggg <haroldtiansheng@gmail.com>

--- starknet-restaking/main.py
+++ starknet-restaking/main.py
@@ -1,9 +1,10 @@
 import os
+from loguru import logger
 
 # 获取一个已存在的环境变量
 home_env = os.getenv('HH')
-print(f"HOME 的值为: {home_env}")
+logger.info(f"HOME 的值为: {home_env}")
+logger.error("error")
 
 # 获取一个不存在的环境变量，并指定默认值
 non_existent_env = os.getenv('HH', '这是一个默认值')
-print(f"MY_CUSTOM_VAR 的值为: {non_existent_env}")
