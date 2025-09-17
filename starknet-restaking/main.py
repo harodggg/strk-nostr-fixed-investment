0cd6dff0a69703197e200e0c52fb9be47686c9c4
harodggg <haroldtiansheng@gmail.com>
2025-09-17 14:40:13 +0800

    feat: add starknet-restaking
    Signed-off-by: harodggg <haroldtiansheng@gmail.com>

--- starknet-restaking/main.py
+++ starknet-restaking/main.py
@@ -1,10 +1,18 @@
 import os
+import asyncio
+from stake import claim_stark
 from loguru import logger
 
-# 获取一个已存在的环境变量
-home_env = os.getenv('HH')
-logger.info(f"HOME 的值为: {home_env}")
-logger.error("error")
 
-# 获取一个不存在的环境变量，并指定默认值
-non_existent_env = os.getenv('HH', '这是一个默认值')
+PRIVATE_KEY = os.getenv(
+    'PRIVATE_KEY', 'you need to set a enviroment variable for private_key')
+logger.info(f"PRIVATE_KEY: {PRIVATE_KEY}")
+ADDRESS = os.getenv(
+    "ADDRESS", "you need to set a enviroment variable for address")
+logger.info(f"ADDRESS: {ADDRESS}")
+
+
+if __name__ == "__main__":
+    logger.info("Over above is ok, now staring...")
+    asyncio.run(claim_stark(
+        {"private_key": PRIVATE_KEY, "address": ADDRESS}))
